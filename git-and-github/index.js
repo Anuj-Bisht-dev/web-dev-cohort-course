066a4bfe9a38f507168cd056cac7ef286c7d7445
Anuj-Bisht-dev <anujbisht.dev@gmail.com>
2026-01-25 22:57:00 +0530

    i have merged this commit from feature/a branch

--- git-and-github/index.js
+++ git-and-github/index.js
@@ -3,5 +3,16 @@ console.log(a);
 
 // i created my new file here
 
-let b  = "singh bisht";
+let b = "singh bisht";
 console.log(b);
+
+function sub(a, b) {
+    try {
+        if (b == 0) {
+            throw new Error("the denominator cannot be zero");
+        }
+        return a - b;
+    } catch {
+        console.log(error.message);
+    }
+}
